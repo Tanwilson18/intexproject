a98fb5c7b0a2f799867bf0af5d48fed10de269af
Tanwilson18 <totannerwilson@gmail.com>
2022-11-28 15:22:53 -0700

    added files

--- kdietapp/views.py
+++ kdietapp/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def indexPageView(request):
+    return render(request, 'trailsapp/index.html')
