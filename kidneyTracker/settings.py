a6a3d6e1f8a7d8b164af61623eee57210975b9cf
Tanwilson18 <totannerwilson@gmail.com>
2022-12-02 00:17:43 -0700

    database?

--- kidneyTracker/settings.py
+++ kidneyTracker/settings.py
@@ -88,7 +88,6 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 DATABASES = {
     "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
-    'Port': '6542',
 }
 
 # LOCAL/OLD DATABASE
