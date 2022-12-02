56d663bd444fd09c065288303b428e1f4604b8ff
Tanwilson18 <totannerwilson@gmail.com>
2022-12-02 01:52:02 -0700

    installed pip >req

--- kidneyTracker/settings.py
+++ kidneyTracker/settings.py
@@ -18,6 +18,7 @@ import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 
@@ -83,23 +84,11 @@ WSGI_APPLICATION = 'kidneyTracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "kdiet",
-        "USER": "postgres",
-        "PASSWORD": "xumVn7KsgDWWFz3rUTfv",
-        "HOST": "containers-us-west-98.railway.app",
-        "PORT": 6092,
-    }
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
-
-# DATABASES = {
-#     "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
-# }
 
 # LOCAL/OLD DATABASE
 # DATABASES = {
