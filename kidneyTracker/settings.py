ecdf4406ca6bf8dbd9421bc7308b7130e99f3332
Tanwilson18 <totannerwilson@gmail.com>
2022-12-01 23:48:22 -0700

    new database live

--- kidneyTracker/settings.py
+++ kidneyTracker/settings.py
@@ -81,15 +81,23 @@ WSGI_APPLICATION = 'kidneyTracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'kdiet',
-        'USER': 'postgres',
-        'PASSWORD': 'Medellin',
-        'HOST': 'localhost',
-    }
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
+
+# LOCAL/OLD DATABASE
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'kdiet',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Medellin',
+#         'HOST': 'localhost',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
